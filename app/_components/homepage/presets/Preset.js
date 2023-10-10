b65351f3f20ae34b409b08d152e2f2422571e817
Abhishek79201 <vaghelaabhishek2580@gmail.com>
2023-10-11 00:53:58 +0530

    preset added

--- app/_components/homepage/presets/Preset.js
+++ app/_components/homepage/presets/Preset.js
@@ -26,6 +26,27 @@ export default function Preset() {
         loop={true}
         freeMode={true}
         modules={[FreeMode]}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          "@0.00": {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          "@0.75": {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          "@1.00": {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          "@1.50": {
+            slidesPerView: 4,
+            spaceBetween: 50,
+          },
+        }}
         className="mySwiper"
       >
         <SwiperSlide>
