feb0ba2c26d4f7bef013e364774b6e821e896ab3
Abhishek79201 <vaghelaabhishek2580@gmail.com>
2023-10-23 03:43:35 +0530

    work3 completed

--- app/(routes)/works/page.js
+++ app/(routes)/works/page.js
@@ -1,14 +1,11 @@
 import LatestWork from "@/app/_components/WorkPage/work";
 import React from "react";
 
-import LatestWork from "@/app/_components/WorkPage/work";
-import React from "react";
-
 const Works = () => {
   return (
     <>
       <div className=" pt-[44px] md:pt-[127px] nxl:pt-[80px]">
-        {/* <LatestWork /> */}
+        <LatestWork />
       </div>
     </>
   );
