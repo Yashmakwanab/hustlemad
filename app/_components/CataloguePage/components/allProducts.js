6d15e70b855867e5ee9eb601c36c46eec2f1c271
Abhishek79201 <vaghelaabhishek2580@gmail.com>
2023-11-04 22:18:11 +0530

    slider added

--- app/_components/CataloguePage/components/allProducts.js
+++ app/_components/CataloguePage/components/allProducts.js
@@ -5,7 +5,10 @@ import ImageWrapper from "../../ImageWrapper/ImageWrapper";
 import { useDispatch, useSelector } from "react-redux";
 import { add } from "../../../redux/slice/cartSlice";
 import { Modal } from "antd";
-import { selectAllProductList, setAllProductList } from "@/app/redux/slice/globleSlice";
+import {
+  selectAllProductList,
+  setAllProductList,
+} from "@/app/redux/slice/globleSlice";
 import CustomOrderModel from "../../models/customOrderModel";
 
 const AllProduct = () => {
@@ -15,21 +18,23 @@ const AllProduct = () => {
   const cartitems = useSelector((state) => state.cart);
   const [open, setOpen] = useState(false);
   const getproducts = async () => {
-    const res = await fetch("https://hustlemad-backend.herokuapp.com/productList");
+    const res = await fetch(
+      "https://hustlemad-backend.herokuapp.com/productList"
+    );
     const data = await res.json();
     setproducts(data.products);
     dispatch(setAllProductList(data.products));
   };
-  const allProducts = useSelector(selectAllProductList)
+  const allProducts = useSelector(selectAllProductList);
 
-  console.log("products", products)
+  console.log("products", products);
 
   useEffect(() => {
     getproducts();
   }, []);
 
   const handleadd = (product) => {
-    console.log("vfhvdsahf", product)
+    console.log("vfhvdsahf", product);
     const isProductInCart = cartitems.some(
       (item) => item?.name === product?.name
     );
@@ -37,70 +42,7 @@ const AllProduct = () => {
       dispatch(add(product));
     }
   };
-  // const modalContainer = (
-  //   <Modal
-  //     centered
-  //     footer={false}
-  //     open={open}
-  //     onCancel={() => setOpen(false)}
-  //     width={800}
-  //   >
-  //     <div className="flex">
-  //       <div>
-  //         <ImageWrapper
-  //           src={"https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg"}
-  //           alt="img"
-  //           className="product_image"
-  //         />
-  //       </div>
-  //       <div>
-  //         <div>
-  //           <h1>The Milk Bottle 2l</h1>
-  //           <h2>HUSTLEMAD</h2>
-  //           Available Colors
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </Modal>
-  // );
-  // const products_content = products.map((product) => (
-  //   <>
-  //     <div key={product._id} className="product_card ">
-  //       <div
-  //         className="product_image_container p-4"
-  //         onClick={() => setOpen(true)}
-  //       >
-  //         <ImageWrapper
-  //           src={product.image}
-  //           alt="img"
-  //           className="product_image"
-  //         />
-  //       </div>
-  //       <p className="font-mazzardSemiBold text-[10px] py-[0.5em] text-white company_title text-center bg-[#0F143A]">
-  //         HUSTLEMAD
-  //       </p>
-  //       <div className="product_detail p-4 flex flex-col gap-1">
-  //         <div className="flex justify-between gap-4">
-  //           <div className="title_container w-[87%]">
-  //             <h4 className="font-mazzardMedium product_title title_container ">
-  //               {product.name}
-  //             </h4>
-  //           </div>
-  //           <div
-  //             className="add_cart flex w-[13%] cursor-pointer"
-  //             onClick={() => handleadd(product)}
-  //           >
-  //             <ImageWrapper
-  //               src={"/Images/categories/plus.webp"}
-  //               className={"w-[24px] h-[24px] cursor-pointer"}
-  //             />
-  //           </div>
-  //         </div>
-  //         <h5 className={"font-mazzard"}>₹ {product.price}</h5>
-  //       </div>
-  //     </div>
-  //   </>
-  // ));
+
   const groupedProducts = allProducts.reduce((acc, product) => {
     if (!acc[product?.categoryId]) {
       acc[product?.categoryId] = [];
@@ -108,20 +50,66 @@ const AllProduct = () => {
     acc[product?.categoryId].push(product);
     return acc;
   }, {});
-
+  function getCategoryName(category) {
+    switch (category) {
+      case "62d964b6f57e16db99387e6c":
+        return "Apparels";
+      case "62d964f7f57e16db99387e6f":
+        return "Drinkware";
+      case "62d9652ef57e16db99387e72":
+        return "Stationery";
+      case "62d96572f57e16db99387e75":
+        return "Accessories";
+      case "62d965b2f57e16db99387e78":
+        return "Games";
+      case "62d965eaf57e16db99387e7b":
+        return "Everything Else";
+      case "62d9662bf57e16db99387e7e":
+        return "Packaging";
+      case "62d966b9f57e16db99387e81":
+        return "Shipping";
+      default:
+        return "";
+    }
+  }
+  function getCategorySubTitle(category) {
+    switch (category) {
+      case "62d964b6f57e16db99387e6c":
+        return "Wear your Hustle";
+      case "62d964f7f57e16db99387e6f":
+        return "Fuel Carriers";
+      case "62d9652ef57e16db99387e72":
+        return "Scribble. Sort. Slay.";
+      case "62d96572f57e16db99387e75":
+        return "Smart tools = Smart tribe!";
+      case "62d965b2f57e16db99387e78":
+        return "No Work. All Play.";
+      case "62d965eaf57e16db99387e7b":
+        return "Outside the box, in the box.";
+      case "62d9662bf57e16db99387e7e":
+        return "The First Impression";
+      case "62d966b9f57e16db99387e81":
+        return "The Last Mile";
+      default:
+        return "";
+    }
+  }
   return (
     <>
-      <div id="drinkware">
-        <div className="flex flex-col items-start">
-          <div>
-            <h1 className="font-recoleta text-[24px] ">Drinkware</h1>
-            <h1 className="font-mazzard text-[36px] ">Fuel up!!</h1>
-          </div>
-          <div className="productsWrapper w-[100%] m-auto cards items-center justify-center nxl:justify-start  py-10">
-            {Object.entries(groupedProducts)?.map(([category, categoryProducts]) => (
-              <div key={category}>
-                <h2>{category}</h2>
-                <div className='productsWrapper flex flex-wrap'>
+      <div className="flex flex-col items-start">
+        <div className="productsWrapper w-[100%] m-auto cards items-center justify-center nxl:justify-start  py-10">
+          {Object.entries(groupedProducts)?.map(
+            ([category, categoryProducts]) => (
+              <div key={category} id={getCategoryName(category)}>
+                <div>
+                  <h1 className="font-recoleta text-[24px] ">
+                    {getCategoryName(category)}
+                  </h1>
+                  <h1 className="font-mazzard text-[36px] pb-4">
+                    {getCategorySubTitle(category)}
+                  </h1>
+                </div>
+                <div className="productsWrapper gap-6 flex flex-wrap">
                   {categoryProducts.map((product) => (
                     <div key={product?._id} className="product_card ">
                       <div
@@ -154,125 +142,31 @@ const AllProduct = () => {
                             />
                           </div>
                         </div>
-                        <h5 className={"font-mazzard"}>₹ {product?.price?.[0]?.cost}</h5>
+                        <h5 className={"font-mazzard"}>
+                          ₹ {product.price[0].cost}
+                        </h5>
                       </div>
                     </div>
                   ))}
-                  <div className="product_card bg-[#0F143A14] cursor-pointer flex items-center" onClick={() => setOpenModel(true)}>
+                  <div
+                    className="product_card bg-[#0F143A14] cursor-pointer flex items-center"
+                    onClick={() => setOpenModel(true)}
+                  >
                     <div className="px-[34px] py-[42px]">
-                    <div className="text-[#0F143A] text-[18px] leading-[27px] font-mazzardSemiBold text-center mb-[16px]">Looking for <br/> something else?</div>
-                    <div className="text-[#0F143ACC] text-[14px] leading-[21px] font-mazzardMedium text-center">If only we could read minds! :D You’ll have to tell us.</div>
+                      <div className="text-[#0F143A] text-[18px] leading-[27px] font-mazzardSemiBold text-center mb-[16px]">
+                        Looking for <br /> something else?
+                      </div>
+                      <div className="text-[#0F143ACC] text-[14px] leading-[21px] font-mazzardMedium text-center">
+                        If only we could read minds! :D You’ll have to tell us.
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
+            )
+          )}
         </div>
       </div>
-      <CustomOrderModel openModel={openModel} setOpenModel={setOpenModel} />
-
-      {/* {modalContainer} */}
-      {/* <div id="apparels">
-        <div className="flex flex-col items-start">
-          <div>
-            <h1 className="font-recoleta text-[24px] ">Apparels</h1>
-            <h1 className="font-mazzard text-[36px] ">Wear your Hustle!</h1>
-          </div>
-          <div className="productsWrapper w-[100%] m-auto cards items-center justify-center nxl:justify-start  py-10">
-            {products_content}
-          </div>
-        </div>
-      </div>
-      <div id="drinkware">
-        <div className="flex flex-col items-start">
-          <div>
-            <h1 className="font-recoleta text-[24px] ">Drinkware</h1>
-            <h1 className="font-mazzard text-[36px] ">Fuel up!!</h1>
-          </div>
-          <div className="productsWrapper w-[100%] m-auto cards items-center justify-center nxl:justify-start  py-10">
-            {products_content}
-          </div>
-        </div>
-      </div>
-      <div id="stationary">
-        <div className="flex flex-col items-start">
-          <div>
-            <h1 className="font-recoleta text-[24px] ">Stationary</h1>
-            <h1 className="font-mazzard text-[36px] ">
-              Scribble . Sort . Slay
-            </h1>
-          </div>
-          <div className="productsWrapper w-[100%] m-auto cards items-center justify-center nxl:justify-start  py-10">
-            {products_content}
-          </div>
-        </div>
-      </div>
-      <div id="accessories">
-        <div className="flex flex-col items-start">
-          <div>
-            <h1 className="font-recoleta text-[24px] ">Stationary</h1>
-            <h1 className="font-mazzard text-[36px] ">
-              Scribble . Sort . Slay
-            </h1>
-          </div>
-          <div className="productsWrapper w-[100%] m-auto cards items-center justify-center nxl:justify-start  py-10">
-            {products_content}
-          </div>
-        </div>
-      </div>
-      <div id="games">
-        <div className="flex flex-col items-start">
-          <div>
-            <h1 className="font-recoleta text-[24px] ">Stationary</h1>
-            <h1 className="font-mazzard text-[36px] ">
-              Scribble . Sort . Slay
-            </h1>
-          </div>
-          <div className="productsWrapper w-[100%] m-auto cards items-center justify-center nxl:justify-start  py-10">
-            {products_content}
-          </div>
-        </div>
-      </div>
-      <div id="everything">
-        <div className="flex flex-col items-start">
-          <div>
-            <h1 className="font-recoleta text-[24px] ">everything</h1>
-            <h1 className="font-mazzard text-[36px] ">
-              Scribble . Sort . Slay
-            </h1>
-          </div>
-          <div className="productsWrapper w-[100%] m-auto cards items-center justify-center nxl:justify-start  py-10">
-            {products_content}
-          </div>
-        </div>
-      </div>
-      <div id="packaging">
-        <div className="flex flex-col items-start">
-          <div>
-            <h1 className="font-recoleta text-[24px] ">packaging</h1>
-            <h1 className="font-mazzard text-[36px] ">
-              Scribble . Sort . Slay
-            </h1>
-          </div>
-          <div className="productsWrapper w-[100%] m-auto cards items-center justify-center nxl:justify-start  py-10">
-            {products_content}
-          </div>
-        </div>
-      </div>
-      <div id="shipping">
-        <div className="flex flex-col items-start">
-          <div>
-            <h1 className="font-recoleta text-[24px] ">Stationary</h1>
-            <h1 className="font-mazzard text-[36px] ">
-              Scribble . Sort . Slay
-            </h1>
-          </div>
-          <div className="productsWrapper w-[100%] m-auto cards items-center justify-center nxl:justify-start  py-10">
-            {products_content}
-          </div>
-        </div>
-      </div> */}
     </>
   );
 };
